c671384ac9c2ca5634d7f3a6132a68d2cbf3bad2
Ruslan-Kostenko <123192295+Ruslan-Kostenko@users.noreply.github.com>
2023-10-19 01:57:16 +0300

    Add some

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,19 @@
 let some:unknown;
 some = 'Text';
-let str: string;
+let str: string | unknown;
 str = some;
 
 export {};
+
+// type size = "small" | "medium" | "large";
+
+// enum Sizes {
+//     s = "small",
+//     m = "medium",
+//     l = "large",
+// };
+
+// const button: size = "large";
+
+// const button2: Sizes = Sizes.l;
+
