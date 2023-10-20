d6be52daccbea0e4602fe0741568f2143a7d9200
Ruslan-Kostenko <123192295+Ruslan-Kostenko@users.noreply.github.com>
2023-10-20 21:39:12 +0300

    Add1

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,19 +1,6 @@
-let some:unknown;
-some = 'Text';
+let some: unknown;
+some = "Text";
 let str: string | unknown;
 str = some;
 
 export {};
-
-// type size = "small" | "medium" | "large";
-
-// enum Sizes {
-//     s = "small",
-//     m = "medium",
-//     l = "large",
-// };
-
-// const button: size = "large";
-
-// const button2: Sizes = Sizes.l;
-
--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -2,16 +2,29 @@
   Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
 */
 
-function showMessage(message) {
+function showMessage(message: string): void {
   console.log(message);
 }
 
-function calc(num1, num2) {
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
-function customError() {
+function customError(): void {
   throw new Error('Error');
 }
 
 export {};
+
+
+// function comby(param1: string | number, param2: string | number) {
+
+// if(typeof param1 === 'number' && param2 === 'number'){
+// return param1 + param2;
+// } else {
+//   if(typeof param1 === 'string' && param2 === 'string') {
+//     return param1.toString() + param2.toString();
+//   }
+// }
+
+// }
