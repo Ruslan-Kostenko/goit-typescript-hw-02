afe2fa29cb4f634ef61eab019caebad96f7a9f66
Ruslan-Kostenko <123192295+Ruslan-Kostenko@users.noreply.github.com>
2023-10-20 23:08:21 +0300

    Completed

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,7 +3,7 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge <T extends Record<string, any>> (objA: T, objB: T): T {
+function merge <R extends object, K extends object>(objA: R, objB: K): object {
   return Object.assign({}, objA, objB);
 }
 
